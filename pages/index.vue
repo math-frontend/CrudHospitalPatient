f3f867b88403ce04f76d5b3a0cbde440194261ea
Matheus Esturiao <matheussturiao@gmail.com>
2023-04-21 13:54:24 -0300

    create pages

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Index {{ storage?.theme }}</p>
+</template>
+
+<script setup>
+const storage = ref(null)
+const listTodo = ref([])
+const todoData = ref({
+  id: 'Abc222',
+  name: '',
+  description: '',
+  done: false
+})
+
+onMounted(() => {
+  storage.value = localStorage
+})
+</script>
